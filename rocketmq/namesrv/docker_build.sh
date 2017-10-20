23f7824f2f5195407de08e7aa0d95abba65dfbb0
putao <513026809@qq.com>
2017-10-20 20:54:17 +0800

    试试

--- rocketmq/namesrv/docker_build.sh
+++ rocketmq/namesrv/docker_build.sh
@@ -1,16 +1,9 @@
 #!/bin/bash 
 
 
-if [ ! -d rocketmq ]; then
-	pushd ..
-		./init.sh
-		mv nogit/rocketmq namesrv/rocketmq
-	popd 
-fi
-
-
-sudo docker build -t bsn069/rocketmq_server:latest .
-
 pushd ..
-	mv namesrv/rocketmq nogit/rocketmq 
-popd 
+	./docker_build.sh
+popd
+
+sudo docker build -t bsn069/rocketmq_namesrv:latest .
+ 
